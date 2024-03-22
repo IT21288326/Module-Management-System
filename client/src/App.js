5443361afef7432bd0c9e6ab615bc19893b0a04c
IT21290992 <158534064+Rusith-S@users.noreply.github.com>
2024-03-22 11:46:18 +0530

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Navigate, Routes, Route} from 'react-router-dom'
 import SidebarSup from './componant/SidebarSup';
 
+
 function App() {
   return (
     <div className="App">
---
+++ client/src/datatablesource.js
@@ -0,0 +1,28 @@
+export const PresentationSheduleColumns = [
+    {
+      field: "groupNo", 
+      headerName: "Group Number", 
+      width: 160,
+    },
+    { 
+      field: "pannelID", 
+      headerName: "Pannel ID", 
+      width: 250 },
+    {
+      field: "date",
+      headerName: "Date",
+      width: 300,
+    },
+  
+    {
+      field: "time",
+      headerName: "Time",
+      width: 200,
+    },
+    {
+      field: "venue",
+      headerName: "Venue",
+      width: 200,
+    },
+    
+  ];
