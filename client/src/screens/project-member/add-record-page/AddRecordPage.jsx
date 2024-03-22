275337ad207ec602a72cc6907b7148f26018473a
IT21290992 <158534064+Rusith-S@users.noreply.github.com>
2024-03-22 21:15:48 +0530

    add record form for presentation shedule

---
+++ client/src/screens/project-member/add-record-page/AddRecordPage.jsx
@@ -0,0 +1,15 @@
+import "./addRecordPage.scss"
+import AddRecord from "../../../componant/project-member/add-presentation-shedule-record-comp/AddPresentationSheduleRecordComp"
+
+const Addrecord = () =>{
+    return(
+        <div className="addrecord_R">        
+            <div className="homeContainer">
+                <h1>Add new Record</h1>
+                <AddRecord/>                
+            </div>
+        </div>
+    )
+}
+
+export default Addrecord
