2e1d62a759cc4278cdf999c6c9fc51ef372da978
Shashi Vinod <it21288326@my.sliit.lk>
2024-03-25 11:13:46 +0530

    signUpForm and modifid User model

--- client/src/App.js
+++ client/src/App.js
@@ -6,8 +6,8 @@ import OTPver from './componant/OTPverify'
 
 import ReportMarks from './screens/supervisor/ReportMarks';
 import Addrecord from './screens/project-member/add-record-page/AddRecordPage';
-
-
+import StudentSignUpForm from './screens/authentication/signupFormstudent';
+import StaffSignUpForm from './screens/authentication/signupFormstaffmembrt';
 
 function App() {
   return (
@@ -20,6 +20,9 @@ function App() {
 
             <Route path='/reportmarks' element={<ReportMarks />} />
             <Route path='/presentation-shedule/addrecord' element={<Addrecord />} />
+            <Route path='/signUp-student' element={<StudentSignUpForm />} />
+
+            <Route path='/signUp-staff' element={<StaffSignUpForm />} />
 
 
 
