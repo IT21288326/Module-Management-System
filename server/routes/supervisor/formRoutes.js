d068bf34042beebc62bcaafe0968dadb0e365ac0
Semini.B.V.S <savidyasemini2001@gmail.com>
2024-03-29 16:54:43 +0530

    server

--- server/routes/supervisor/formRoutes.js
+++ server/routes/supervisor/formRoutes.js
@@ -8,6 +8,6 @@ const router = express.Router();
 // Route to handle form submission
 router.post('/submit-form', submitFormData);
 router.get('/reports', getReports);
-router.get('/group/:reportIndex/:studentIndex', getGroupDetails);
-router.put('/reports/:reportIndex/:studentIndex', updateCoSupervisorMarks);
+// router.get('/group/:reportIndex/:studentIndex', getGroupDetails);
+// router.put('/reports/:reportIndex/:studentIndex', updateCoSupervisorMarks);
 export default router;
