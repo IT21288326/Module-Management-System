ab2b7323bbc18edcf9279265064ef6010eb45079
Semini.B.V.S <99323453+IT21342394@users.noreply.github.com>
2024-05-10 10:48:14 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -33,8 +33,9 @@ import SidebarCoSup from './componant/SidebarCoSupervisor';
 import Edit from './screens/co-supervisor/edit';
 import backgroundImage from './img/back1.jpg';
 import MarkSheet from './screens/supervisor/AssesmentMarks';
-import MarkingForm from './screens/supervisor/MarkingForm'
-
+import MarkingForm from './screens/supervisor/MarkingForm';
+import MarkingTableNew from './screens/supervisor/MarkingTableNew';
+import Update from './screens/supervisor/Update';
 function App() {
   return (
     <div className="app" style={{ background: '-webkit-linear-gradient(90deg, hsla(186, 33%, 94%, 1) 0%, hsla(216, 41%, 79%, 1) 100%)'}}>
@@ -72,7 +73,9 @@ function App() {
             <Route path='/co-sup' element={<SidebarCoSup/>}/>
             <Route path='/edit' element={<Edit/>}/>
             <Route path='/last' element={<MarkingForm/>}/>
-            
+            <Route path='/lastta' element={<MarkingTableNew/>}/>
+            <Route path='/lastup' element={<Update/>}/>
+
           {/* Rusith */}
             <Route path='/projMemberSideBar' element={<SidebarProjMember />} />
             
